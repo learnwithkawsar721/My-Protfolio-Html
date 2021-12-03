b7dd57b1505d6dcdab48ad61405620548a75e533
learnwithkawsar <kawsar721g@gmail.com>
2021-12-03 21:31:59 +0600

    hero Section done

---
+++ assets/js/TypeJs/type.function.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".element", {
+  strings: ["Full Stack Developer", "MERN Stack Developer"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  smartBackspace: true, // this is a default
+  loop: true,
+  showCursor: true,
+  cursorChar: "|",
+  autoInsertCss: true,
+});
